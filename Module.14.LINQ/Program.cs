6d4f42ec082fad9293ff4dfba15e5ea9fbd0b72e
DE-corp <crazyHOPHOP@rambler.ru>
2023-04-03 20:23:10 +0300

    Задание 14.1.2

--- Module.14.LINQ/Program.cs
+++ Module.14.LINQ/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Module._14.LINQ
@@ -7,30 +8,19 @@ namespace Module._14.LINQ
     {
         static void Main(string[] args)
         {
-            var russianCities = new List<City>();
-            russianCities.Add(new City("Москва", 11900000));
-            russianCities.Add(new City("Санкт-Петербург", 4991000));
-            russianCities.Add(new City("Волгоград", 1099000));
-            russianCities.Add(new City("Казань", 1169000));
-            russianCities.Add(new City("Севастополь", 449138));
+            string[] text = { "Раз два три",
+                "четыре пять шесть",
+                "семь восемь девять" };
 
-            var cities = russianCities.Where(c => c.Name.Length <= 10).OrderByDescending(c => c.Name.Length);
+            var words = from str in text // пробегаемся по элементам массива
+                        from word in str.Split(' ') // дробим каждый элемент по пробелам, сохраняя в новую последовательность
+                        select word; // собираем все куски в результирующую выборку
 
-            foreach (var city in cities)
-                System.Console.WriteLine(city.Name + " - " + city.Population);
+            // выводим результат
+            foreach (var word in words)
+                Console.WriteLine(word);
+
         }
 
-    }
-
-    public class City
-    {
-        public City(string name, long population)
-        {
-            Name = name;
-            Population = population;
-        }
-
-        public string Name { get; set; }
-        public long Population { get; set; }
     }
 }
