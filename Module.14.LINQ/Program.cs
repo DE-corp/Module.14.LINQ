a78892f725ccc565f2225495c636090af0075d1b
DE-corp <crazyHOPHOP@rambler.ru>
2023-04-03 21:21:47 +0300

    Задание 14.2.3

--- Module.14.LINQ/Program.cs
+++ Module.14.LINQ/Program.cs
@@ -6,18 +6,44 @@ namespace Module._14.LINQ
 {
     class Program
     {
+        class Student
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public List<string> Languages { get; set; }
+
+        }
+
+        public class Application
+        {
+            public string Name { get; set; }
+            public int YearOfBirth { get; set; }
+        }
+
         static void Main(string[] args)
         {
-            string[] words = { "Обезьяна", "Лягушка", "Кот", "Собака", "Черепаха" };
-
-            var newList = words.Select(w => new
+            List<Student> students = new List<Student>
             {
-                Name = w,
-                Count = w.Length
-            }).OrderByDescending(w => w.Count);
+               new Student {Name="Андрей", Age=23, Languages = new List<string> {"английский", "немецкий" }},
+               new Student {Name="Сергей", Age=27, Languages = new List<string> {"английский", "французский" }},
+               new Student {Name="Дмитрий", Age=29, Languages = new List<string> {"английский", "испанский" }},
+               new Student {Name="Василий", Age=24, Languages = new List<string> {"испанский", "немецкий" }}
+            };
 
-            foreach (var word in newList)
-                Console.WriteLine(word.Name + " " + word.Count);
+
+            var selectedStudents = from s in students
+                                   where s.Age < 27
+                                   // временная переменная для генерации полного имени
+                                   let yearOfBirth = DateTime.Now.Year - s.Age
+                                   // проекция в новую сущность с использованием новой переменной
+                                   select new Application
+                                   {
+                                       Name = s.Name,
+                                       YearOfBirth = yearOfBirth
+                                   };
+
+            foreach (var stud in selectedStudents)
+                Console.WriteLine(stud.Name + ", " + stud.YearOfBirth);
         }
     }
 }
