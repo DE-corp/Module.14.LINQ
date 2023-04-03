05ab27178ebc3d39d1c3e28090ee2085c01870cd
DE-corp <crazyHOPHOP@rambler.ru>
2023-04-03 21:34:49 +0300

    Задание 14.2.4

--- Module.14.LINQ/Program.cs
+++ Module.14.LINQ/Program.cs
@@ -13,37 +13,41 @@ namespace Module._14.LINQ
             public List<string> Languages { get; set; }
 
         }
-
-        public class Application
+        class Coarse
         {
             public string Name { get; set; }
-            public int YearOfBirth { get; set; }
+            public DateTime StartDate { get; set; }
         }
 
         static void Main(string[] args)
         {
-            List<Student> students = new List<Student>
+            // Список студентов
+            var students = new List<Student>
             {
                new Student {Name="Андрей", Age=23, Languages = new List<string> {"английский", "немецкий" }},
                new Student {Name="Сергей", Age=27, Languages = new List<string> {"английский", "французский" }},
-               new Student {Name="Дмитрий", Age=29, Languages = new List<string> {"английский", "испанский" }},
-               new Student {Name="Василий", Age=24, Languages = new List<string> {"испанский", "немецкий" }}
+               new Student {Name="Дмитрий", Age=29, Languages = new List<string> {"английский", "испанский" }}
             };
 
+            // Список курсов
+            var coarses = new List<Coarse>
+            {
+               new Coarse {Name="Язык программирования C#", StartDate = new DateTime(2020, 12, 20)},
+               new Coarse {Name="Язык SQL и реляционные базы данных", StartDate = new DateTime(2020, 12, 15)},
+            };
 
-            var selectedStudents = from s in students
-                                   where s.Age < 27
-                                   // временная переменная для генерации полного имени
-                                   let yearOfBirth = DateTime.Now.Year - s.Age
-                                   // проекция в новую сущность с использованием новой переменной
-                                   select new Application
-                                   {
-                                       Name = s.Name,
-                                       YearOfBirth = yearOfBirth
-                                   };
+            // добавим студентов в курсы
+            var studentsWithCoarses = from stud in students
+                                      where stud.Age <= 29
+                                      where stud.Languages.Contains("английский")
+                                      let birth = DateTime.Now.Year - stud.Age
+                                      from coarse in coarses
+                                      where coarse.Name == "Язык программирования C#"
+                                      select new { Name = stud.Name, BirthYear = birth, CoarseName = coarse.Name };
 
-            foreach (var stud in selectedStudents)
-                Console.WriteLine(stud.Name + ", " + stud.YearOfBirth);
+            // выведем результат
+            foreach (var stud in studentsWithCoarses)
+                Console.WriteLine($"Студент {stud.Name} {stud.BirthYear} добавлен в курс {stud.CoarseName}");
         }
     }
 }
