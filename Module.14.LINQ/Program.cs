d80e189a242a7cfc20bd66dfdbfdfc9dce616560
DE-corp <crazyHOPHOP@rambler.ru>
2023-04-03 19:28:04 +0300

    Задание 14.1.1

--- Module.14.LINQ/Program.cs
+++ Module.14.LINQ/Program.cs
@@ -7,18 +7,30 @@ namespace Module._14.LINQ
     {
         static void Main(string[] args)
         {
-            var objects = new List<object>()
-            {
-               1,
-               "Сергей ",
-               "Андрей ",
-               300,
-            };
+            var russianCities = new List<City>();
+            russianCities.Add(new City("Москва", 11900000));
+            russianCities.Add(new City("Санкт-Петербург", 4991000));
+            russianCities.Add(new City("Волгоград", 1099000));
+            russianCities.Add(new City("Казань", 1169000));
+            russianCities.Add(new City("Севастополь", 449138));
 
-            var names = objects.Where(n => n is string).OrderBy(n => n);
+            var cities = russianCities.Where(c => c.Name.Length <= 10).OrderByDescending(c => c.Name.Length);
 
-            foreach (string name in names)
-                System.Console.WriteLine(name);
+            foreach (var city in cities)
+                System.Console.WriteLine(city.Name + " - " + city.Population);
         }
+
+    }
+
+    public class City
+    {
+        public City(string name, long population)
+        {
+            Name = name;
+            Population = population;
+        }
+
+        public string Name { get; set; }
+        public long Population { get; set; }
     }
 }
